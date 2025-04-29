679ec43886e56eda58a667662f60df9b115a8764
szamosi.mate <szamimate@yahoo.com>
2025-04-29 15:41:37 +0200

    - the quit action can now terminate process

--- src/multiconn_archicad/actions/connection_manager.py
+++ src/multiconn_archicad/actions/connection_manager.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
-from multiconn_archicad.errors import RequestError, ArchicadAPIError
 from multiconn_archicad.conn_header import is_id_initialized
 
 if TYPE_CHECKING:
@@ -20,7 +19,7 @@ class ConnectionManager(ABC):
         self.multi_conn: MultiConn = multi_conn
 
     def from_ports(self, *args: Port) -> list[ConnHeader]:
-        return self.execute_action(
+        return self._execute_action(
             [
                 self.multi_conn.open_port_headers[port]
                 for port in args
@@ -29,17 +28,17 @@ class ConnectionManager(ABC):
         )
 
     def from_headers(self, *args: ConnHeader) -> list[ConnHeader]:
-        return self.execute_action([*args])
+        return self._execute_action([*args])
 
     def all(self) -> list[ConnHeader]:
-        return self.execute_action(list(self.multi_conn.open_port_headers.values()))
+        return self._execute_action(list(self.multi_conn.open_port_headers.values()))
 
     @abstractmethod
-    def execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]: ...
+    def _execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]: ...
 
 
 class Connect(ConnectionManager):
-    def execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]:
+    def _execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]:
         for conn_header in conn_headers:
             project_name = (
                 conn_header.archicad_id.projectName if is_id_initialized(conn_header.archicad_id) else "Unknown"
@@ -49,30 +48,15 @@ class Connect(ConnectionManager):
         return conn_headers
 
     def failed(self) -> None:
-        self.execute_action(list(self.multi_conn.failed.values()))
+        self._execute_action(list(self.multi_conn.failed.values()))
 
 
 class Disconnect(ConnectionManager):
-    def execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]:
+    def _execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]:
         for conn_header in conn_headers:
             project_name = (
                 conn_header.archicad_id.projectName if is_id_initialized(conn_header.archicad_id) else "Unknown"
             )
             log.info(f"Disconnecting from project {project_name} at port {conn_header.port}")
             conn_header.disconnect()
-        return conn_headers
-
-
-class QuitAndDisconnect(ConnectionManager):
-    def execute_action(self, conn_headers: list[ConnHeader]) -> list[ConnHeader]:
-        for conn_header in conn_headers:
-            if conn_header.port:
-                log.info(f"Sending Quit command to Archicad on port {conn_header.port} and unassigning header.")
-                try:
-                    conn_header.core.post_tapir_command("QuitArchicad")
-                except (RequestError, ArchicadAPIError) as e:
-                    log.error(f"Failed to quit archicad: error: {e}")
-                    raise
-                self.multi_conn.open_port_headers.pop(conn_header.port)
-            conn_header.unassign()
         return conn_headers
