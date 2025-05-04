c858e3212a82056001ced9d6faf5bae019b35c13
Szamosi Máté <62789148+SzamosiMate@users.noreply.github.com>
2025-05-04 21:24:39 +0200

    Quit action can terminate the process is the JSON command fails (#22)
    * - the quit action can now terminate process
    
    * corrected typing and import mistakes

--- src/multiconn_archicad/actions/__init__.py
+++ src/multiconn_archicad/actions/__init__.py
@@ -1,6 +1,7 @@
-from .connection_manager import Connect, QuitAndDisconnect, Disconnect
+from .connection_manager import Connect, Disconnect
 from .project_handler import FindArchicad, OpenProject, SwitchProject
 from .refresh import Refresh
+from .quit import QuitAndDisconnect
 
 __all__: tuple[str, ...] = (
     "Connect",
