7e75ee85aa9639b582d6b95ae7e20dd2fc980204
SzamosiMate <szamimate@yahoo.com>
2025-10-06 20:21:05 +0200

    - created a new base model "APIModel" for each base model. This allowed the removal of the redundant ModelConfigs from the models.

--- code_generation/official/model_generators/04_generate_dicts_and_models.py
+++ code_generation/official/model_generators/04_generate_dicts_and_models.py
@@ -1,18 +1,21 @@
 from datamodel_code_generator import DataModelType, InputFileType, generate, LiteralType
 from code_generation.official.paths import official_paths
 
+CUSTOM_BASE_CLASS = "multiconn_archicad.models.base.APIModel"
+
 print("Generating Pydantic models")
 generate(
     official_paths.MASTER_SCHEMA_OUTPUT,
     input_file_type=InputFileType.JsonSchema,
     output=official_paths.RAW_PYDANTIC_MODELS,
     output_model_type=DataModelType.PydanticV2BaseModel,
+    base_class=CUSTOM_BASE_CLASS,
     enum_field_as_literal=LiteralType.One,
     use_union_operator=True,
     use_double_quotes=True,
     collapse_root_models=True,
     field_constraints=True,
-    use_annotated=True
+    use_annotated=True,
 )
 
 print("Generating Typed Dicts")
--- code_generation/tapir/model_generators/02_generate_dicts_and_models.py
+++ code_generation/tapir/model_generators/02_generate_dicts_and_models.py
@@ -1,12 +1,15 @@
 from datamodel_code_generator import DataModelType, InputFileType, generate, LiteralType
 from code_generation.tapir.paths import paths
 
+CUSTOM_BASE_CLASS = "multiconn_archicad.models.base.APIModel"
+
 print("Generating Pydantic models")
 generate(
     paths.MASTER_SCHEMA_OUTPUT,
     input_file_type=InputFileType.JsonSchema,
     output=paths.RAW_PYDANTIC_MODELS,
     output_model_type=DataModelType.PydanticV2BaseModel,
+    base_class=CUSTOM_BASE_CLASS,
     enum_field_as_literal=LiteralType.One,
     use_union_operator=True,
     use_double_quotes=True,
