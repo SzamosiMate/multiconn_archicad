312573cfce3ce7ba58077509aa1221826fb8ba7e
SzamosiMate <szamimate@yahoo.com>
2026-08-19 22:34:35 +0200

    feat: dump by alias and exclude none by default when dumping a model

--- src/multiconn_archicad/models/base.py
+++ src/multiconn_archicad/models/base.py
@@ -1,7 +1,36 @@
+from typing import Any
 from pydantic import BaseModel, ConfigDict
 
 class APIModel(BaseModel):
-    """A custom base model that forbids extra (unexpected) fields by default."""
+    """A custom base model for the Unified API"""
     model_config = ConfigDict(
         extra="forbid",
+        populate_by_name=True,
+        serialize_by_alias=True,
     )
+
+    def model_dump(
+        self,
+        *,
+        by_alias: bool = True,
+        exclude_none: bool = True,
+        **kwargs: Any,
+    ) -> dict[str, Any]:
+        return super().model_dump(
+            by_alias=by_alias,
+            exclude_none=exclude_none,
+            **kwargs,
+        )
+
+    def model_dump_json(
+        self,
+        *,
+        by_alias: bool = True,
+        exclude_none: bool = True,
+        **kwargs: Any,
+    ) -> str:
+        return super().model_dump_json(
+            by_alias=by_alias,
+            exclude_none=exclude_none,
+            **kwargs,
+        )
