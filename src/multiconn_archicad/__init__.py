1586d051fc6839f798aa4f82b5519f4566c4edfb
szamosi.mate <szamimate@yahoo.com>
2026-09-17 13:20:59 +0200

    refactor: rebased onto main

--- src/multiconn_archicad/__init__.py
+++ src/multiconn_archicad/__init__.py
@@ -5,6 +5,26 @@ import logging
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
+    from multiconn_archicad.clients.core.core_commands import CoreCommands
+    from multiconn_archicad.clients.standard_connection import StandardConnection
+    from multiconn_archicad.clients.unified_api.api import UnifiedApi
+    from multiconn_archicad.errors import (
+        AddOnCommandUnavailable,
+        APIConnectionError,
+        APIErrorBase,
+        ArchicadAPIError,
+        BatchOperationError,
+        CommandTimeoutError,
+        InvalidResponseFormatError,
+        MulticonnArchicadError,
+        NotFullyInitializedError,
+        ProjectAlreadyOpenError,
+        ProjectNotFoundError,
+        RequestError,
+        StandardAPIError,
+        StandardCommandUnavailable,
+        TapirCommandError,
+    )
     from multiconn_archicad.orchestration.basic_types import (
         APIResponseError,
         ArchicadLocation,
@@ -30,33 +50,13 @@ if TYPE_CHECKING:
         is_session_ready,
         is_tapir_session_ready,
     )
-    from multiconn_archicad.clients.core.core_commands import CoreCommands
     from multiconn_archicad.orchestration.dialog_handlers import (
         DialogHandlerBase,
         UnhandledDialogError,
         WinDialogHandler,
         win_int_handler_factory,
     )
-    from multiconn_archicad.errors import (
-        AddOnCommandUnavailable,
-        APIConnectionError,
-        APIErrorBase,
-        ArchicadAPIError,
-        CommandTimeoutError,
-        InvalidResponseFormatError,
-        MulticonnArchicadError,
-        NotFullyInitializedError,
-        ProjectAlreadyOpenError,
-        ProjectNotFoundError,
-        RequestError,
-        StandardAPIError,
-        StandardCommandUnavailable,
-        TapirCommandError,
-        BatchOperationError,
-    )
     from multiconn_archicad.orchestration.multi_conn import MultiConn
-    from multiconn_archicad.clients.standard_connection import StandardConnection
-    from multiconn_archicad.clients.unified_api.api import UnifiedApi
 
 
 __all__ = [
@@ -107,40 +107,40 @@ __all__ = [
 ]
 
 _LAZY_IMPORTS: dict[str, str] = {
-    # .multi_conn
-    "MultiConn": ".multi_conn",
-    # .conn_header
-    "ConnHeader": ".conn_header",
-    "ProjectIdentityHeader": ".conn_header",
-    "SessionReadyHeader": ".conn_header",
-    "ValidatedHeader": ".conn_header",
-    "has_project_identity": ".conn_header",
-    "is_session_ready": ".conn_header",
-    "is_tapir_session_ready": ".conn_header",
-    "is_header_fully_initialized": ".conn_header",
-    "is_id_initialized": ".conn_header",
-    "is_location_initialized": ".conn_header",
-    "is_product_info_initialized": ".conn_header",
-    # .basic_types
-    "ArchiCadID": ".basic_types",
-    "TeamworkProjectID": ".basic_types",
-    "SoloProjectID": ".basic_types",
-    "UntitledProjectID": ".basic_types",
-    "TeamworkCredentials": ".basic_types",
-    "ProductInfo": ".basic_types",
-    "ArchicadLocation": ".basic_types",
-    "Port": ".basic_types",
-    "APIResponseError": ".basic_types",
-    "TapirInfo": ".basic_types",
-    # .standard_connection
-    "StandardConnection": ".standard_connection",
-    # .core.core_commands
-    "CoreCommands": ".core.core_commands",
-    # .dialog_handlers
-    "DialogHandlerBase": ".dialog_handlers",
-    "UnhandledDialogError": ".dialog_handlers",
-    "WinDialogHandler": ".dialog_handlers",
-    "win_int_handler_factory": ".dialog_handlers",
+    # .orchestration.multi_conn
+    "MultiConn": ".orchestration.multi_conn",
+    # .orchestration.conn_header
+    "ConnHeader": ".orchestration.conn_header",
+    "ProjectIdentityHeader": ".orchestration.conn_header",
+    "SessionReadyHeader": ".orchestration.conn_header",
+    "ValidatedHeader": ".orchestration.conn_header",
+    "has_project_identity": ".orchestration.conn_header",
+    "is_session_ready": ".orchestration.conn_header",
+    "is_tapir_session_ready": ".orchestration.conn_header",
+    "is_header_fully_initialized": ".orchestration.conn_header",
+    "is_id_initialized": ".orchestration.conn_header",
+    "is_location_initialized": ".orchestration.conn_header",
+    "is_product_info_initialized": ".orchestration.conn_header",
+    # .orchestration.basic_types
+    "ArchiCadID": ".orchestration.basic_types",
+    "TeamworkProjectID": ".orchestration.basic_types",
+    "SoloProjectID": ".orchestration.basic_types",
+    "UntitledProjectID": ".orchestration.basic_types",
+    "TeamworkCredentials": ".orchestration.basic_types",
+    "ProductInfo": ".orchestration.basic_types",
+    "ArchicadLocation": ".orchestration.basic_types",
+    "Port": ".orchestration.basic_types",
+    "APIResponseError": ".orchestration.basic_types",
+    "TapirInfo": ".orchestration.basic_types",
+    # .clients.standard_connection
+    "StandardConnection": ".clients.standard_connection",
+    # .clients.core.core_commands
+    "CoreCommands": ".clients.core.core_commands",
+    # .orchestration.dialog_handlers
+    "DialogHandlerBase": ".orchestration.dialog_handlers",
+    "UnhandledDialogError": ".orchestration.dialog_handlers",
+    "WinDialogHandler": ".orchestration.dialog_handlers",
+    "win_int_handler_factory": ".orchestration.dialog_handlers",
     # .errors
     "MulticonnArchicadError": ".errors",
     "APIErrorBase": ".errors",
@@ -157,8 +157,8 @@ _LAZY_IMPORTS: dict[str, str] = {
     "ProjectNotFoundError": ".errors",
     "NotFullyInitializedError": ".errors",
     "BatchOperationError": ".errors",
-    # .unified_api.api
-    "UnifiedApi": ".unified_api.api",
+    # .clients.unified_api.api
+    "UnifiedApi": ".clients.unified_api.api",
 }
 
 
